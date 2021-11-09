5c8495a2f74f3cf5aad5bce0e7973e813cb832f9
hello@timrickaby.com <hello@timrickaby.com>
2021-11-09 15:47:19 +0000

    Portfolio updates.

--- index.php
+++ index.php
@@ -24,7 +24,6 @@
 							<p>Logoform A. Modelled on a blues waveform from the Healey amplifier.</p>
 						</div>
 					</div>
-
 					<div class="column-1-2">
 						<div class="item" data-type="embed">
 							<div class="media-wrapper">
@@ -56,6 +55,145 @@
 				</div>
 			</div>
 		</div>
+
+
+		<div class="project">
+			<div class="item" data-type="copy">
+				<h2>Adidas P.O.D System Retail Launch</h2>
+				<h3>Retail and point of sale. Concepts, design and production guidelines.</h3>
+				<h4>(IMA / Adidas)</h4>
+			</div>
+			
+			<div class="item" data-type="image">
+				<img src="http://modocodo.test/work/adidas-pod-01.jpg" width="100%" />
+			</div>
+	
+			<div class="row">
+				<div class="column-1-3">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-09.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-3">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-10.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-3">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-11.jpg" width="100%" />
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-03.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-04.jpg" width="100%" />
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-05.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-06.jpg" width="100%" />
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-07.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-pod-08.jpg" width="100%" />
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="project">
+			<div class="item" data-type="copy">
+				<h2>Haworth Festival</h2>
+				<h3>Brand identity, print design and website.<br />
+				Inspired by the well-worn cobbles and contemporary culture.</h3>
+			</div>
+			<div class="row">
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/haworth-festival-logo.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/haworth-festival-logo.jpg" width="100%" />
+					</div>
+				</div>
+			</div>
+			<div class="item" data-type="image">
+				<img src="http://modocodo.test/work/haworth-festival-posters.jpg" width="100%" />
+			</div>
+			<div class="item" data-type="image">
+				<img src="http://modocodo.test/work/haworth-festival-collateral.jpg" width="100%" />
+			</div>
+		</div>
+		
+
+		<div class="project">
+			<div class="item" data-type="copy">
+				<h2>Adidas Originals – Nite Jogger VR Experience</h2>
+				<h3>Concept and design of worldwide in-store product launch activation.</h3>
+				<h4>(IMA)</h4>
+			</div>
+			<div class="item" data-type="embed">
+				<div class="media-wrapper">
+					<iframe src="https://player.vimeo.com/video/330599851" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="caption">
+					<p>3D environment produced for Occulus Rift headsets.</p>
+				</div>
+			</div>
+			<div class="item" data-type="image">
+				<img src="http://modocodo.test/work/adidas-nite-jogger-vr-flat-render.jpg" width="100%" />
+				<div class="caption">
+					<p>Flat visual of the 3D environment.</p>
+				</div>
+			</div>
+			<?php /*
+			<div class="row">
+				<div class="column-1-3">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-nite-jogger-instore-01.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-3">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-nite-jogger-instore-02.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-3">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/adidas-nite-jogger-instore-03.jpg" width="100%" />
+					</div>
+				</div>
+			</div>
+			*/ ?>
+		</div>
+
+
 
 		<div class="project">
 			<div class="item" data-type="copy">
@@ -97,134 +235,6 @@
 			</div>
 		</div>
 
-
-
-		<div class="project">
-			<div class="item" data-type="copy">
-				<h2>Adidas P.O.D System Retail Launch</h2>
-				<h3>Retail and point of sale. Concepts, design and production guidelines.</h3>
-				<h4>(IMA / Adidas)</h4>
-			</div>
-			
-			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/adidas-pod-01.jpg" width="100%" />
-			</div>
-	
-			<div class="row">
-				<div class="column-1-3">
-					<div class="item" data-type="image">
-						<img src="http://modocodo.test/work/adidas-pod-09.jpg" width="100%" />
-					</div>
-				</div>
-				<div class="column-1-3">
-					<div class="item" data-type="image">
-						<img src="http://modocodo.test/work/adidas-pod-10.jpg" width="100%" />
-					</div>
-				</div>
-				<div class="column-1-3">
-					<div class="item" data-type="image">
-						<img src="http://modocodo.test/work/adidas-pod-11.jpg" width="100%" />
-					</div>
-				</div>
-			</div>
-
-			<div class="item">
-				<div class="row">
-					<div class="column-1-2">
-						<div class="item" data-type="image">
-							<img src="http://modocodo.test/work/adidas-pod-03.jpg" width="100%" />
-						</div>
-					</div>
-					<div class="column-1-2">
-						<div class="item" data-type="image">
-							<img src="http://modocodo.test/work/adidas-pod-04.jpg" width="100%" />
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="column-1-2">
-						<div class="item" data-type="image">
-							<img src="http://modocodo.test/work/adidas-pod-05.jpg" width="100%" />
-						</div>
-					</div>
-					<div class="column-1-2">
-						<div class="item" data-type="image">
-							<img src="http://modocodo.test/work/adidas-pod-06.jpg" width="100%" />
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="column-1-2">
-						<div class="item" data-type="image">
-							<img src="http://modocodo.test/work/adidas-pod-07.jpg" width="100%" />
-						</div>
-					</div>
-					<div class="column-1-2">
-						<div class="item" data-type="image">
-							<img src="http://modocodo.test/work/adidas-pod-08.jpg" width="100%" />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="project">
-			<div class="item" data-type="copy">
-				<h2>Haworth Festival</h2>
-				<h3>Brand identity, print design and website.<br />
-				Inspired by the well-worn cobbles and contemporary culture.</h3>
-			</div>
-			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/haworth-festival-logo.jpg" width="100%" />
-			</div>
-			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/haworth-festival-posters.jpg" width="100%" />
-			</div>
-			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/haworth-festival-collateral.jpg" width="100%" />
-			</div>
-		</div>
-		
-		<div class="project">
-			<div class="item" data-type="copy">
-				<h2>Adidas Originals – Nite Jogger VR Experience</h2>
-				<h3>Concept and design of worldwide in-store product launch activation.</h3>
-				<h4>(IMA)</h4>
-			</div>
-			<div class="item" data-type="embed">
-				<div class="media-wrapper">
-					<iframe src="https://player.vimeo.com/video/330599851" frameborder="0" allowfullscreen></iframe>
-				</div>
-				<div class="caption">
-					<p>3D environment produced for Occulus Rift headsets.</p>
-				</div>
-			</div>
-			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/adidas-nite-jogger-vr-flat-render.jpg" width="100%" />
-				<div class="caption">
-					<p>Flat visual of the 3D environment.</p>
-				</div>
-			</div>
-			<?php /*
-			<div class="row">
-				<div class="column-1-3">
-					<div class="item" data-type="image">
-						<img src="http://modocodo.test/work/adidas-nite-jogger-instore-01.jpg" width="100%" />
-					</div>
-				</div>
-				<div class="column-1-3">
-					<div class="item" data-type="image">
-						<img src="http://modocodo.test/work/adidas-nite-jogger-instore-02.jpg" width="100%" />
-					</div>
-				</div>
-				<div class="column-1-3">
-					<div class="item" data-type="image">
-						<img src="http://modocodo.test/work/adidas-nite-jogger-instore-03.jpg" width="100%" />
-					</div>
-				</div>
-			</div>
-			*/ ?>
-		</div>
 
 		<div class="project">
 			<div class="item" data-type="copy">
@@ -320,8 +330,17 @@
 			<div class="item" data-type="image">
 				<img src="http://modocodo.test/work/awesome-exporter-ui-01.jpg" width="100%" />
 			</div>
-			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/awesome-exporter-logo-01.jpg" width="100%" />
+			<div class="row">
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/oliver-hunt-logo.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/oliver-hunt-logo.jpg" width="100%" />
+					</div>
+				</div>
 			</div>
 		</div>
 
@@ -330,13 +349,20 @@
 				<h2>Oliver Hunt</h2>
 				<h3>Web design and brand identity for PGA professional golfer.</h3>
 			</div>
-			<div class="item">
-				<div class="item" data-type="image">
-					<img src="http://modocodo.test/work/oliver-hunt-mobile-website-01.jpg" width="100%" />
+			<div class="row">
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/oliver-hunt-logo.jpg" width="100%" />
+					</div>
 				</div>
-				<div class="item" data-type="image">
-					<img src="http://modocodo.test/work/oliver-hunt-logo.jpg" width="100%" />
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/oliver-hunt-logo.jpg" width="100%" />
+					</div>
 				</div>
+			</div>
+			<div class="item" data-type="image">
+				<img src="http://modocodo.test/work/oliver-hunt-mobile-website-01.jpg" width="100%" />
 			</div>
 		</div>
 
@@ -345,13 +371,11 @@
 				<h2>Musicins Centre</h2>
 				<h3>Web design and brand identity for PGA professional golfer.</h3>
 			</div>
-			<div class="item">
-				<div class="item" data-type="image">
-					<img src="http://modocodo.test/work/musicians-centre-logo.jpg" width="100%" />
-				</div>
-				<div class="item" data-type="image">
-					<img src="http://modocodo.test/work/musicians-centre-posters.jpg" width="100%" />
-				</div>
+			<div class="item" data-type="image">
+				<img src="http://modocodo.test/work/musicians-centre-logo.jpg" width="100%" />
+			</div>
+			<div class="item" data-type="image">
+				<img src="http://modocodo.test/work/musicians-centre-posters.jpg" width="100%" />
 			</div>
 		</div>
 
@@ -374,33 +398,57 @@
 			<div class="item" data-type="image">
 				<img src="http://modocodo.test/work/logos-atticus.jpg" width="100%" />
 			</div>
-
-			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/logos-doofman.jpg" width="100%" />
-			</div>
 			
-			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/logos-emma-stone-photography.jpg" width="100%" />
-					<div class="caption">
-						<p>Emma Stone – Portrait Photographer</p>
+			<div class="item" data-type="copy">
+				<h2>&nbsp;</h2>
+				<h3>Allianz Resqyou Breakdown Identity</h2>
+			</div>
+			<div class="row">
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/logos-resqyou-01.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/logos-resqyou-02.jpg" width="100%" />
+					</div>
 				</div>
 			</div>
 
 			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/logos-aire-valley-archers.jpg" width="100%" />
+				<img src="http://modocodo.test/work/logos-emma-stone-photography.jpg" width="100%" />
+				<div class="caption">
+					<p>Emma Stone – Portrait Photographer</p>
+				</div>
+			</div>
+
+			<div class="item" data-type="copy">
+				<h2>&nbsp;</h2>
+				<h3>Aire Valley Archers</h2>
+			</div>
+			<div class="row">
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/logos-aire-valley-archers-01.jpg" width="100%" />
+					</div>
+				</div>
+				<div class="column-1-2">
+					<div class="item" data-type="image">
+						<img src="http://modocodo.test/work/logos-aire-valley-archers-02.jpg" width="100%" />
+					</div>
+				</div>
 			</div>
 
 			<div class="item" data-type="image">
 				<img src="http://modocodo.test/work/logos-hayim.jpg" width="100%" />
 				<div class="caption">
-				<p>Hayim Apparel</p>
+					<p>Hayim Apparel</p>
+				</div>
 			</div>
 
 			<div class="item" data-type="image">
-				<img src="http://modocodo.test/work/logos-resqyou.jpg" width="100%" />
-				<div class="caption">
-					<p>Aire Valley Archers – Archery Club</p>
-				</div>
+				<img src="http://modocodo.test/work/logos-doofman.jpg" width="100%" />
 			</div>
 		</div>
 	</div>
