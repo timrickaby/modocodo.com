7dc4a680709118f7efaee0cd04e0f7093e7ed929
Tim Rickaby <hello@timrickaby.com>
2022-05-22 19:20:19 +0100

    Profile updates.

--- about.php
+++ about.php
@@ -2,17 +2,18 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
 
 	<div id="body" class="row <?php echo $location; ?>">
-		<p>Modocodo is the creative studio of <a href="https://timrickaby.com" target="_blank">Tim Rickaby</a> – Yorkshire based freelance designer and coder.</p>
+		<p>Modocodo is the creative studio of <a href="https://timrickaby.com" target="_blank">Tim Rickaby</a> – Yorkshire based freelance designer and developer.</p>
 
 		<p>Working agency-side with brands like Adidas and Reebok, plus in-house with many wonderful SME's, 
-		family businesses, startups, charities and musicians to deliver unique, meaningful and effective creative and technical solutions for domestic and global markets.</p>
+		family businesses, startups, charities and musicians to deliver unique, meaningful and effective solutions for domestic and global markets.</p>
 		
-		<p>Modocodo blends design, code and words to establish and elevate brands, products and people.</p>
+		<p>Through design, code and words, Modocodo blends creativity and technology to elevate brands, products and people.</p>
 
 		<h2>Available for remote bookings.</h2>
 		<p><a href="mailto:hello@modocodo.studio" target="_blank">hello@modocodo.studio</a></p>
+		<p><a href="tel:+447786432440">+44 (0)7786 432440</a></p>
 
-		<h2>Studio services:</h2>
+		<h2>Studio Services</h2>
 		<ul>
 			<li><p>Creative direction</p></li>
 			<li><p>Brand identity design</p></li>
@@ -21,13 +22,13 @@
 			<li><p>Short-form copywriting</p></li>
 			<li><p>Product photography</p></li>
 			<li><p>Creative artworking</p></li>
-			<li><p>UI design</p></li>
 			<li><p>Asset library creation</p></li>
+			<li><p>UI design</p></li>
 			<li><p>Web design and development</p></li>
 			<li><p>Web hosting and management</p></li>
 		</ul>
 
-		<h2>Links:</h2>
+		<h2>Links</h2>
 		<p><a href="https://www.linkedin.com/in/timrickaby" target="_blank">LinkedIn</a></p>
 		<p><a href="https://github.com/timrickaby" target="_blank">GitHub</a></p>
 		<p><a href="https://instagram.com/modocodo.studio" target="_blank">Instagram</a></p>
